19fe5fce6d784b2c010125151eb8c86d0060411e
Disha <suledisha1@ccs.neu.edu>
2018-06-24 13:24:20 -0400

    Added public profile functions

--- services/authoredbook.service.server.js
+++ services/authoredbook.service.server.js
@@ -3,6 +3,8 @@ module.exports = function (app) {
     app.post('/api/authoredbook/:bookId', addAuthoredBook);
     app.delete('/api/authoredbook/:bookId', removeAuthoredBook);
     app.get('/api/book/authors', findAuthoredBooksForUser);
+    app.get('/api/user/:userId/authored', findAuthoredBooksUserById);
+
     var authoredBookModel = require('../models/authoredBook/authoredBook.model.server');
 
     function addAuthoredBook(req, res) {
@@ -21,6 +23,15 @@ module.exports = function (app) {
     function findAuthoredBooksForUser(req, res) {
         var currentUser = req.session.currentUser;
         var userId = currentUser._id;
+        authoredBookModel
+            .findAuthoredBooksForUser(userId)
+            .then(function(authors) {
+                res.json(authors);
+            });
+    }
+
+    function findAuthoredBooksUserById(req, res) {
+        var userId = req.params.userId;
         authoredBookModel
             .findAuthoredBooksForUser(userId)
             .then(function(authors) {
--- services/like.service.server.js
+++ services/like.service.server.js
@@ -3,6 +3,7 @@ module.exports = function (app) {
     app.get('/api/book/likes', findLikedBooksForUser);
     app.post('/api/book/:bookId/likes', userLikesBook);
     app.delete('/api/book/:bookId/unlikes', userUnlikesBook);
+    app.get('/api/user/:userId/liked', findLikedBooksUserById);
 
     var bookModel = require('../models/book/book.model.server');
     var likeModel = require('../models/like/like.model.server');
@@ -10,6 +11,15 @@ module.exports = function (app) {
     function findLikedBooksForUser(req, res) {
         var currentUser = req.session.currentUser;
         var userId = currentUser._id;
+        likeModel
+            .findLikedBooksForUser(userId)
+            .then(function(likes) {
+                res.json(likes);
+            });
+    }
+
+    function findLikedBooksUserById(req, res) {
+        var userId = req.params.userId;
         likeModel
             .findLikedBooksForUser(userId)
             .then(function(likes) {
